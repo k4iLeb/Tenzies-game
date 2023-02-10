5537754e4bdd02df8eb2abe598d648c37920c71f
k4iLeb <k4ilebi@gmail.com>
2023-02-10 18:41:55 +0200

    added holdDice, add Conditional CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { nanoid } from "nanoid";
 import React from "react";
 import "./App.css";
 import Die from "./components/Die";
@@ -8,15 +9,37 @@ function App() {
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push(Math.ceil(Math.random() * 6));
+      const die = {
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid(),
+      };
+      newDice.push(die);
     }
+    // console.log(newDice);
     return newDice;
   }
 
-  const diceElements = dice.map((item) => <Die value={item} />);
+  const diceElements = dice.map((die) => (
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
+  ));
 
   function rollDice() {
     setDice(allNewDice);
+  }
+
+  function holdDice(id) {
+    // change isHeld=true when clicked
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
   }
 
   return (
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,8 +1,12 @@
 import "../App.css";
 
 export default function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#6be494" : "#ffffff",
+  };
+
   return (
-    <div className="die">
+    <div onClick={props.holdDice} style={styles} className="die">
       <h2 className="die-num">{props.value}</h2>
     </div>
   );
