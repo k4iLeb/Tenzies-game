ec58a0abd88028c0e214a60948babefb14f7f2ff
k4iLeb <k4ilebi@gmail.com>
2023-02-11 19:27:13 +0200

    Added trackers & game state

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,20 @@ import Die from "./components/Die";
 function App() {
   const [dice, setDice] = React.useState(allNewDice);
   const [tenzies, setTenzies] = React.useState(false);
+  const [rolls, setRolls] = React.useState(0);
+  const [game, setGame] = React.useState(false);
+  const [counter, setCounter] = React.useState(0);
+  const [highScore, setHighScore] = React.useState("-");
+  const [lastScore, setLastScore] = React.useState(0);
+
+  useEffect(() => {
+    if (game && !tenzies) {
+      var timer = setInterval(() => {
+        setCounter(counter + 1);
+      }, 1000);
+    }
+    return () => clearInterval(timer);
+  }, [game, tenzies, counter]);
 
   React.useEffect(() => {
     const allHeld = dice.every((die) => die.isHeld);
@@ -29,7 +43,6 @@ function App() {
     for (let i = 0; i < 10; i++) {
       newDice.push(generateNewDie());
     }
-    // console.log(newDice);
     return newDice;
   }
 
@@ -43,31 +56,69 @@ function App() {
   ));
 
   function rollDice() {
-    if (!tenzies) {
-      setDice((oldDice) =>
-        oldDice.map((die) => {
-          return die.isHeld ? die : generateNewDie();
-        })
-      );
+    if (!game) {
+      setGame((oldGame) => !oldGame);
     } else {
-      setTenzies(false);
-      setDice(allNewDice);
+      if (!tenzies) {
+        setDice((oldDice) =>
+          oldDice.map((die) => {
+            return die.isHeld ? die : generateNewDie();
+          })
+        );
+        countRolls();
+      } else {
+        setTenzies(false);
+        setDice(allNewDice);
+        setRolls(0);
+        setGame(false);
+        setLastScore(counter);
+        if (highScore === "-") {
+          setHighScore(counter);
+        }
+        if (counter < highScore) {
+          setHighScore(counter);
+        }
+        setCounter(0);
+      }
     }
   }
 
   function holdDice(id) {
-    // change isHeld=true when clicked
-    setDice((oldDice) =>
-      oldDice.map((die) => {
-        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
-      })
-    );
+    if (!game) {
+    } else {
+      // change isHeld=true when clicked
+      setDice((oldDice) =>
+        oldDice.map((die) => {
+          return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+        })
+      );
+    }
   }
+
+  // *** ROLL COUNTER ***
+  function countRolls() {
+    setRolls((oldRolls) => oldRolls + 1);
+  }
+
+  // *** TIMER ***
+  const styles = {
+    backgroundColor: game ? "#572dfd" : "#6be494",
+  };
 
   return (
     <div className="Container">
       {tenzies && <Confetti />}
       <main>
+        <div className="nav">
+          <div className="scores">
+            <p className="highScore">Last try: {lastScore} (s)</p>
+            <p className="highScore">Best try: {highScore} (s)</p>
+          </div>
+          <div className="trackers">
+            <p className="timePassed">Time: {counter} (s)</p>
+            <p className="rollCount">Rolls: {rolls}</p>
+          </div>
+        </div>
         <h1 className="title">Tenzies</h1>
         <p className="instructions">
           Roll until all dice are the same. Click each die to freeze it at its
@@ -79,8 +130,9 @@ function App() {
           name="button"
           className="roll-btn"
           value="Roll"
+          style={styles}
         >
-          {tenzies ? "NEW GAME" : "Roll"}
+          {!game ? "START GAME" : tenzies ? "NEW GAME" : "Roll"}
         </button>
       </main>
     </div>
